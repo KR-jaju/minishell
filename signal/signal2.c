ee45e90f95c061f691c7eb475ff5f08f3717451b
KR-jaju <wnwoals0127@gmail.com>
2023-08-08 17:11:55 +0900

    jaeyojun

---
+++ signal/signal2.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaju <jaju@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 13:42:56 by jaju              #+#    #+#             */
+/*   Updated: 2023/08/08 15:11:46 by jaju             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "signal.h"
+
+int	ft_nbrlen(long n)
+{
+	int		size;
+
+	size = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		size++;
+	while (n != 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
+
+int	ft_power(int nbr, int pow)
+{
+	int	res;
+
+	if (pow == 0)
+		return (1);
+	res = nbr;
+	while (pow > 1)
+	{
+		res *= nbr;
+		pow--;
+	}
+	return (res);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nbr;
+	char	*s;
+	int		div;
+	int		i;
+
+	i = 0;
+	nbr = n;
+	s = malloc(sizeof(char) * (ft_nbrlen(nbr) + 1));
+	if (!s)
+		return (NULL);
+	if (nbr < 0)
+	{
+		s[i++] = '-';
+		nbr = -nbr;
+	}
+	div = ft_power(10, ft_nbrlen(nbr) - 1);
+	while (div > 0)
+	{
+		s[i++] = nbr / div + '0';
+		nbr = nbr % div;
+		div = div / 10;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+void	sig_quit(int code)
+{
+	char	*nbr;
+
+	nbr = ft_itoa(code);
+	write(2, "^\\Quit: ", 8);
+	write(2, nbr, str_length(nbr));
+	write(2, "\n", 1);
+	if (nbr)
+	{
+		free(nbr);
+		nbr = NULL;
+	}
+}
